632d87c10ab3688d67cb7107368ebe35c853d2fd
wwyf <w.y.ff@163.com>
2020-11-17 23:02:43 +0800

    update aufs

--- src3-aufs/readme.md
+++ src3-aufs/readme.md
@@ -2,3 +2,5 @@
 
 
 follow this [link](https://kukuruku.co/post/writing-a-file-system-in-linux-kernel/)
+
+and this [link](https://github.com/krinkinmu/aufs)
--- src3-aufs/super.c
+++ src3-aufs/super.c
@@ -1,14 +1,93 @@
+#include <linux/buffer_head.h>
+#include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/slab.h>
+
+// #include "aufs.h"
+static const unsigned long AUFS_MAGIC = 0x13131313;
+
+
+static void aufs_put_super(struct super_block *sb)
+{
+    pr_debug("aufs super block destroyed\n");
+}
+
+static struct super_operations const aufs_super_ops = {
+    .put_super = aufs_put_super,
+};
+
+static int aufs_fill_sb(struct super_block *sb, void *data, int silent)
+{
+    struct inode *root = NULL;
+
+    sb->s_magic = AUFS_MAGIC;
+    sb->s_op = &aufs_super_ops;
+
+    root = new_inode(sb);
+    if (!root)
+    {
+        pr_err("inode allocation failed\n");
+        return -ENOMEM;
+    }
+
+    root->i_ino = 0;
+    root->i_sb = sb;
+    // root->i_atime = root->i_mtime = root->i_ctime = 4;
+    inode_init_owner(root, NULL, S_IFDIR);
+
+    sb->s_root = d_make_root(root);
+    if (!sb->s_root)
+    {
+        pr_err("root creation failed\n");
+        return -ENOMEM;
+    }
+
+    return 0;
+}
+
+static struct dentry *aufs_mount(struct file_system_type *type, int flags,
+                                 char const *dev, void *data)
+{
+    struct dentry *entry = mount_bdev(type, flags, dev, data, aufs_fill_sb);
+
+    if (IS_ERR(entry))
+        pr_err("aufs mounting failed\n");
+    else
+        pr_debug("aufs mounted\n");
+    return entry;
+}
+
+static struct file_system_type aufs_type = {
+    .owner = THIS_MODULE,
+    .name = "aufs",
+    .mount = aufs_mount,
+    .kill_sb = kill_block_super,
+    .fs_flags = FS_REQUIRES_DEV,
+};
 
 static int __init aufs_init(void)
 {
+
+    int ret = register_filesystem(&aufs_type);
+    if (ret != 0)
+    {
+        pr_err("cannot register filesystem\n");
+        return ret;
+    }
+
     pr_debug("aufs module loaded\n");
     return 0;
 }
 
 static void __exit aufs_fini(void)
 {
+    int ret = unregister_filesystem(&aufs_type);
+
+    if (ret != 0)
+    {
+        pr_err("cannot unregister filesystem\n");
+    }
     pr_debug("aufs module unloaded\n");
 }
 
