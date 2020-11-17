0fa74b6d5c177bdf47b4808b3fef25465d5bbfc4
wwyf <w.y.ff@163.com>
2020-11-17 17:47:51 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# linux kernel module
+
+2020-11-17 17:43:57
+
+follow this [link](https://zhuanlan.zhihu.com/p/31931538) to write my linux kernel module.
---
+++ scripts/init-env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get install build-essential linux-headers-`uname -r` -y
+git config --global user.name "wwyf"
+git config --global user.email "w.y.ff@163.com"
